59aa6f0536efe8a2e78e6635a5be04af2813ee5e
Dima Stopel <dima@stopel.org>
2012-12-23 10:55:59 +0200

    initial

---
+++ component1/FibCalc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace winrtComponentNS
+{
+    public sealed class FibCalculator
+    {
+        int _a;
+        // Just an example for non default constructor. It is called from JS.
+        public FibCalculator(int a)
+        {
+            _a = a;
+        }
+
+        public int Fib(int n) 
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return Fib(n - 1) + Fib(n - 2);
+            }
+        }
+    }
+}
